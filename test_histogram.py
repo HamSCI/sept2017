08ba932ad521b77faeb2fa7ad398eb430b333c41
Evan Markowitz <evan.markowitz99@gmail.com>
2018-06-06 17:51:20 -0400

    This is totally broken, please don't touch it, it is only here for synchronization purposes

--- test_histogram.py
+++ test_histogram.py
@@ -17,11 +17,8 @@ import json
 CONFIG_FILE = ""
 BIN_SCALE = 1.
 
-
-
-
-config = get_config(CONFIG_FILE)
-prmd = setup_prmd(config)
+# config = get_config(CONFIG_FILE)
+# prmd = setup_prmd(config)
 
 # I'm going to explain this here in the event someone else comes upon
 # this file before I'm done with it.  I'm trying out a different config
@@ -53,12 +50,37 @@ def setup_prmd(config):
 
 def get_bins(lim, bin_size):
     """ Helper function to split a limit into bins of the proper size """
-    return np.arrange(lim[0], lim[1]+2*bin_size, bin_size)
+    return np.arange(lim[0], lim[1]+2*bin_size, bin_size)
 
 
 if __name__ == "__main__":
     pass
     # x-axis: UTC
     xbins = get_bins((0, 24), 10./60)
+    print(xbins)
     # y-axis: distance (km)
     ybins = get_bins((0, 3000), 500)
+
+    df = pd.read_csv("csvs/2017-09-05.csv.bz2")
+    df["occurred"] = pd.to_datetime(df["occurred"])
+    df = df.loc[df["source"] == 1]
+    hist, xb, yb = np.histogram2d(df["occurred"].dt.hour, df["dist_Km"], bins=[xbins, ybins])
+    
+    print(hist)
+
+    fig = plt.figure(figsize=(7, 3))
+    ax = fig.add_subplot(111, title='TESTING.')
+
+    vmin    = 0
+    vmax    = 0.8*np.max(hist)
+    if np.sum(hist) == 0:
+        vmax = 1.0
+
+    cmap    = plt.cm.jet
+    # pcoll   = ax.contourf(xb[:-1],yb[:-1],hist.T,15,vmin=vmin,vmax=vmax,cmap=cmap)
+    print(df[["occurred", "dist_Km"]].head(1))
+    pcoll   = df.plot.scatter("occurred", "dist_Km", ax=ax)
+    # cbar    = plt.colorbar(pcoll,ax=ax)
+    # cbar.set_label('Spot Density')
+
+    plt.savefig("maps/" +"TEST"+ ".png")
