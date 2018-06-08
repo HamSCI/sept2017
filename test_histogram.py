158b26fff6cb51e5861d0884e190af0c9e7b0b9d
Evan Markowitz <evan.markowitz99@gmail.com>
2018-06-08 14:40:31 -0400

    Refactor, set the default to make histograms for the whole month, needs formatting and cleanup

--- test_histogram.py
+++ test_histogram.py
@@ -5,6 +5,7 @@ matplotlib.use('Agg')
 
 import numpy as np
 import pandas as pd
+import datetime
 import cartopy.crs as ccrs
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
@@ -12,41 +13,11 @@ import sys
 import time
 
 import geopack
-import json
+from timeutils import daterange
 
-CONFIG_FILE = ""
-BIN_SCALE = 1.
+CSV_FILE_PATH = "csvs/{}.csv.bz2"
+BANDS = [160,80,40,20,15,10]
 
-# config = get_config(CONFIG_FILE)
-# prmd = setup_prmd(config)
-
-# I'm going to explain this here in the event someone else comes upon
-# this file before I'm done with it.  I'm trying out a different config
-# setup inspired by rx_superposed_epoch.py which is meant to make it
-# easier for non-programmers to generate graphs while also keeping
-# configuration data away from sourcecode for readibility purposes
-#
-# I will probably end up changing this around as needed, but for now
-# this is how I'm doing it :)
-
-def get_config(config_file):
-    """ Quick and dirty loader function """
-    with open(config_file, "r") as f:
-        config = json.load(f)
-
-def setup_prmd(config):
-    """ Creates a parameter dictionary from the JSON config """
-    prmd = []
-    for i, j in config["histograms"]:
-        if "lim" in j:
-            j["lim"] = tuple(j["lim"])
-        if "bin_size" in j:
-            j["bin_size"] *= BIN_SCALE
-            j["bins"] = get_bins(j["lim"], j["bin_size"])
-        if "cmap" in j:
-            j["cmap"] = matplotlib.cm.get_cmap(j["cmap"])
-        prmd[i] = j
-    return prmd
 
 def get_bins(lim, bin_size):
     """ Helper function to split a limit into bins of the proper size """
@@ -57,22 +28,19 @@ def hours_from_dt64(dt64, date_):
     return (dt64 - date_).astype(float) / 3600
 
 
-if __name__ == "__main__":
-    pass
+def make_histogram_from_dataframe(df: pd.DataFrame, ax: matplotlib.axes.Axes, title: str):
+    # TODO: Make all of this stuff configurable
+    # Ultimately the goal is for this to be very versatile
     # x-axis: UTC
     xbins = get_bins((0, 24), 10./60)
     # y-axis: distance (km)
     ybins = get_bins((0, 3000), 500)
 
-    df = pd.read_csv("csvs/2017-09-05.csv.bz2")
-    # df = df.head(10000)
-    df["occurred"] = pd.to_datetime(df["occurred"])
-    df["occurred_hr"] = hours_from_dt64(df["occurred"].values.astype("M8[s]"), np.datetime64("2017-09-05"))
-    df = df.loc[df["band"] == 20]
+
+    # TODO: Clean this bit up, namely the hours_from_dt64 setup
     hist, xb, yb = np.histogram2d(df["occurred_hr"], df["dist_Km"], bins=[xbins, ybins])
 
-    fig = plt.figure(figsize=(7, 3))
-    ax = fig.add_subplot(111, title='2017-09-05 (20m)')
+    ax.set_title(title)
 
     # "borrowed" from SEQP
     vmin    = 0
@@ -85,4 +53,23 @@ if __name__ == "__main__":
     cbar    = plt.colorbar(pcoll,ax=ax)
     cbar.set_label('Spot Density')
 
-    plt.savefig("maps/" +"TEST"+ ".png")
+
+def make_histograms_by_date(date_str: str):
+    df = pd.read_csv(CSV_FILE_PATH.format(date_str))
+    df["occurred"] = pd.to_datetime(df["occurred"])
+    df["occurred_hr"] = hours_from_dt64(df["occurred"].values.astype("M8[s]"), np.datetime64(date_str))
+    fig = plt.figure(figsize=(7, 3.5*len(BANDS)))
+    for i, band in enumerate(BANDS):
+        frame = df.loc[df["band"] == band]
+        ax = fig.add_subplot(len(BANDS), 1, i+1)
+        title = date_str + " (" + str(band) + "m)"
+        make_histogram_from_dataframe(frame, ax, title)
+    fig.savefig("hist/" + date_str + ".png")
+
+if __name__ == "__main__":
+    sDate = datetime.datetime(2017, 9, 1)
+    eDate = datetime.datetime(2017, 10, 1)
+    for dt in daterange(sDate, eDate):
+        print("Making histogram for", dt)
+        date_str = dt.strftime("%Y-%m-%d")
+        make_histograms_by_date(date_str)
