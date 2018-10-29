8e703deac3e45734e99778c69500b602b6c0ac27
Nathaniel A. Frissell <nathaniel.a.frissell@njit.edu>
2018-10-29 12:23:21 -0400

    more omni data

--- library/omni.py
+++ library/omni.py
@@ -1,5 +1,9 @@
 import datetime
 import glob
+
+from collections import OrderedDict
+import bz2
+
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
@@ -11,26 +15,144 @@ from .symh import SymH
 def to_ut_hr(dt):
     return dt.hour + dt.minute/60. + dt.second/3600.
 
-class Omni():
-    def __init__(self,years=[2016,2017]):
-        self._load_omni()
-        self._load_symh(years)
+def date_parser(row):
+    year    = row['year']
+    doy     = row['doy']
+    hr      = row['hr']
 
-    def _load_omni(self,omni_csv='data/omni/omni2_3051.csv'):
+    dt  = datetime.datetime(int(year),1,1)+datetime.timedelta(days=(int(doy)-1),hours=int(hr))
+    return dt
+
+class Omni():
+    def __init__(self):
+        self._load_omni()
+        self._load_symh()
+
+    def _load_omni(self,omni_csv='data/omni/omni_data_reduced.txt.bz2'):
         """
         Load OMNI data.
         """
 
-        omni_df         = pd.read_csv(omni_csv,
-                            parse_dates={'datetime':['year','doy','hr']},
-                            date_parser=self._date_parser)
-        omni_df         = omni_df.set_index('datetime')
-        omni_df['Kp']   = omni_df['Kp_x10']/10.
-        del omni_df['Kp_x10']
+        input_file  = omni_csv
 
-        self.df         = omni_df
+        vrs = OrderedDict()
+        vrs['Bartels rotation number']          = {'symbol':'bartels'}
+        vrs['ID for IMF spacecraft']            = {'symbol':'IMF_spacecraft_ID',        'NaN':99}
+        vrs['ID for SW Plasma spacecraft']      = {'symbol':'SW_spacecraft_ID',         'NaN':99}
+        vrs['# of points in IMF averages']      = {'symbol':'N_IMF_points',             'NaN':999}
+        vrs['# of points in Plasma averag.']    = {'symbol':'N_plasma_points',          'NaN':999}
+        vrs['Scalar B, nT']                     = {'symbol':'B_nT',                     'NaN':999.9}
+        vrs['Vector B Magnitude,nT']            = {'symbol':'B_vec_nT',                 'NaN':999.9}
+        vrs['Lat. Angle of B (GSE)']            = {'symbol':'B_theta_GSE',              'NaN':999.9}
+        vrs['Long. Angle of B (GSE)']           = {'symbol':'B_phi_GSE',                'NaN':999.9}
+        vrs['BX, nT (GSE, GSM)']                = {'symbol':'Bx_GSE_nT',                'NaN':999.9}
+        vrs['BY, nT (GSE)']                     = {'symbol':'By_GSE_nT',                'NaN':999.9}
+        vrs['BZ, nT (GSE)']                     = {'symbol':'Bz_GSE_nT',                'NaN':999.9}
+        vrs['BY, nT (GSM)']                     = {'symbol':'By_GSM_nT',                'NaN':999.9}
+        vrs['BZ, nT (GSM)']                     = {'symbol':'Bz_GSM_nT',                'NaN':999.9}
+        vrs['RMS_magnitude, nT']                = {'symbol':'RMS_mag_nT',               'NaN':999.9}
+        vrs['RMS_field_vector, nT']             = {'symbol':'RMS_vec_nT',               'NaN':999.9}
+        vrs['RMS_BX_GSE, nT']                   = {'symbol':'RMS_BX_GSE_nT',            'NaN':999.9}
+        vrs['RMS_BY_GSE, nT']                   = {'symbol':'RMS_BY_GSE_nT',            'NaN':999.9}
+        vrs['RMS_BZ_GSE, nT']                   = {'symbol':'RMS_BZ_GSE_nT',            'NaN':999.9}
+        vrs['SW Plasma Temperature, K']         = {'symbol':'T_K',                      'NaN':9999999}
+        vrs['SW Proton Density, N/cm^3']        = {'symbol':'N_p',                      'NaN':999.9}
+        vrs['SW Plasma Speed, km/s']            = {'symbol':'V',                        'NaN':9999}
+        vrs['SW Plasma flow long. angle']       = {'symbol':'SW_phi',                   'NaN':999.9}
+        vrs['SW Plasma flow lat. angle']        = {'symbol':'SW_theta',                 'NaN':999.9}
+        vrs['Alpha/Prot. ratio']                = {'symbol':'alpha_prot_ratio',         'NaN':9.999}
+        vrs['sigma-T,K']                        = {'symbol':'sigma_T_K',                'NaN':9999999}
+        vrs['sigma-n, N/cm^3)']                 = {'symbol':'sigma_N_p',                'NaN':999.9}
+        vrs['sigma-V, km/s']                    = {'symbol':'sigma_V',                  'NaN':9999}
+        vrs['sigma-phi V, degrees']             = {'symbol':'sigma_phi',                'NaN':999.9}
+        vrs['sigma-theta V, degrees']           = {'symbol':'sigma_theta',              'NaN':999.9}
+        vrs['sigma-ratio']                      = {'symbol':'sigma_ratio',              'NaN':9.999}
+        vrs['Flow pressure']                    = {'symbol':'P',                        'NaN':99.99}
+        vrs['E elecrtric field']                = {'symbol':'E',                        'NaN':999.99}
+        vrs['Plasma betta']                     = {'symbol':'beta',                     'NaN':999.99}
+        vrs['Alfen mach number']                = {'symbol':'M_alfven',                 'NaN':999.9}
+        vrs['Magnetosonic Much num.']           = {'symbol':'M_magsonic',               'NaN':99.9}
+        vrs['Kp index']                         = {'symbol':'Kp_x10'}
+        vrs['R (Sunspot No.)']                  = {'symbol':'R'}
+        vrs['Dst-index, nT']                    = {'symbol':'Dst_nT'}
+        vrs['ap_index, nT']                     = {'symbol':'Ap_nT'}
+        vrs['f10.7_index']                      = {'symbol':'F10.7'}
+        vrs['AE-index, nT']                     = {'symbol':'AE',                       'NaN':9999}
+        vrs['AL-index, nT']                     = {'symbol':'AL',                       'NaN':99999}
+        vrs['AU-index, nT']                     = {'symbol':'AU',                       'NaN':99999}
+        vrs['pc-index']                         = {'symbol':'Pc',                       'NaN':999.9}
+        vrs['Lyman_alpha']                      = {'symbol':'Lyman_alpha'}
+        vrs['Proton flux (>1 Mev)']             = {'symbol':'PF_1MeV',                  'NaN':999999.99}
+        vrs['Proton flux (>2 Mev)']             = {'symbol':'PF_2MeV',                  'NaN':99999.99}
+        vrs['Proton flux (>4 Mev)']             = {'symbol':'PF_4MeV',                  'NaN':99999.99}
+        vrs['Proton flux (>10 Mev)']            = {'symbol':'PF_10MeV',                 'NaN':99999.99}
+        vrs['Proton flux (>30 Mev)']            = {'symbol':'PF_30MeV',                 'NaN':99999.99}
+        vrs['Proton flux (>60 Mev)']            = {'symbol':'PF_60MeV',                 'NaN':99999.99}
+        vrs['Flux FLAG']                        = {'symbol':'flux_flag'}
 
-    def _load_symh(self,years=None):
+
+        # Open File
+        if input_file[-4:] == '.bz2':
+            with bz2.BZ2File(input_file) as fl:
+                lines   = fl.readlines()
+            lines   = [x.decode() for x in lines]
+
+        else:
+            with open(input_file) as fl:
+                lines   = fl.readlines()
+        
+        # Pull out data, ignore headers and footers.
+        data    = []
+        names   = OrderedDict()
+        for line in lines:
+            ln  = line.split()
+            try:
+                if ln[0] == 'YEAR':
+                    cols = ln
+                    continue
+
+                col0 = int(ln[0])
+                if col0 > 1800:
+                    data.append(ln)
+                else:
+                    names[col0] = ' '.join(ln[1:])
+            except:
+                pass
+
+        # Get a dictionary that only has the variables in the actual datafile.
+        columns = ['year','doy','hr']
+        nan_dct = OrderedDict()
+        for key,val in names.items():
+            result = vrs.get(val)
+            if result is None:
+                symbol = key
+            else:
+                symbol          = result['symbol']
+                nan_dct[symbol] = result.get('NaN')
+            columns.append(symbol)
+
+        # Place into dataframe.
+        df          = pd.DataFrame(data,columns=columns,dtype=np.float)
+
+        # Parse Dates and remove old date columns
+        df.index    = df.apply(date_parser,axis=1)
+        del df['year']
+        del df['doy']
+        del df['hr']
+
+        # Remove Not-a-Numbers
+        for key,val in nan_dct.items():
+            if val is not None:
+                tf  = df[key] == val
+                df.loc[tf,key]  = np.nan
+
+        # Scale Kp
+        df['Kp']   = df['Kp_x10']/10.
+        del df['Kp_x10']
+
+        self.df         = df 
+
+    def _load_symh(self,years=[2016,2017]):
         """
         Create and load SymH data/object.
         """
@@ -38,12 +160,6 @@ class Omni():
         self.symh       = symh
         self.df_symasy  = symh.df
 
-    def _date_parser(self,years,doys,hrs):
-        datetimes = []
-        for year,doy,hr in zip(years,doys,hrs):
-            dt  = datetime.datetime(int(year),1,1)+datetime.timedelta(days=(int(doy)-1),hours=int(hr))
-            datetimes.append(dt)
-        return datetimes
 
     def plot_dst_kp(self,sTime,eTime,ax,xkey='index',xlabels=True,
             dst_param='Dst_nT',dst_lw=1,kp_markersize=10):
